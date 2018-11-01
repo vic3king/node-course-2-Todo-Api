d22f775abbb1bec0606224e5f3f6d89afb8c43d8
vic3king <vic3coorp@gmail.com>
2018-11-01 19:00:29 +0000

    Add test for GET/todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,46 +1,53 @@
-//load in saved dependencies
-const expect = require('expect')
-const request = require('supertest')
+// load in saved dependencies
+const expect = require('expect');
+const request = require('supertest');
 
-//load in requires files for testing from server and models
-const { app } = require('./../server')
-const { Todo } = require('./../models/todo')
+// load in requires files for testing from server and models
+const { app } = require('./../server');
+const { Todo } = require('./../models/todo');
 
-//test lifecycle method lets us run a code before every test case, in our case make sure the db is empty
+// add todos for testing 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
+// test lifecycle method lets us run a code before every test case, in our case make sure the db is empty
 beforeEach((done) => {
   Todo.deleteOne({}).then(() => {
-    return done()
-  })
-})
+    return Todo.insertMany(todos);
+  }).then(() => done());
+});
 
-//describe to group all test cases
+// describe to group all test cases
 describe('POST /todos', () => {
-  //verify that when data is sent everything works
+  //  verify that when data is sent everything works
   it('should create a new todo', (done) => {
-    const text = 'Test todo text'
+    var text = 'Test todo text';
 
-    //supper test request the app file
+    //  supper test request the app file
     request(app)
       .post('/todos')
       .send({ text })
       .expect(200)
       .expect((res) => {
-        expect(res.body.text).toBe(text)
+        expect(res.body.text).toBe(text);
       })
       .end((err, res) => {
         if (err) {
-          return done(err)
+          return done(err);
         }
 
-        Todo.find().then((todos) => {
-          expect(todos.length).toBe(1)
-          expect(todos[0].text).toBe(text)
-          done()
-        }).catch((e) => done(e))
-      })
-  })
+        Todo.find({ text }).then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
 
-  //test to check if body data is valid
+  //  test to check if body data is valid
   it('should not create todo with invalid body data', (done) => {
     request(app)
       .post('/todos')
@@ -48,14 +55,28 @@ describe('POST /todos', () => {
       .expect(400)
       .end((err, res) => {
         if (err) {
-          return done(err)
+          return done(err);
         }
-        //fetch from database and test
-        Todo.find().then((todos) => {
-          expect(todos.length).toBe(0)
-          done()
-        }).catch((e) => done(e))
-      })
-  })
 
-})
+        //  fetch from database and test
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(2);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
+
+// test for Get route
+describe('GET /todos', () => {
+  // test to make sure that when a valid id is passed in it returns the id
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
+});
